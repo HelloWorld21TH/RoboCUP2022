ee75ca81096319e6d8e18d6d06fd55b7b2d912d3
sh0kul <104113257+sh0kuI@users.noreply.github.com>
2022-05-23 20:29:10 +0700

    Update TestServer.py

--- Control Raspberry Pi/TestServer.py
+++ Control Raspberry Pi/TestServer.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import zmq
 
-context = zmq.Context()
+context =  zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 GET = ""
@@ -16,43 +16,42 @@ GPIO.setup(23, GPIO.OUT)
 GPIO.setup(29, GPIO.OUT)
 #---PWM---
 GPIO.setup(36, GPIO.OUT)
-
+PWMBo = GPIO.PWM(36, 50)
 
 #Left A
 GPIO.setup(31, GPIO.OUT)
 GPIO.setup(33, GPIO.OUT)
 #---PWM---
 GPIO.setup(38, GPIO.OUT)
+PWMA = GPIO.PWM(38, 50)
 
 #Right B
 GPIO.setup(35, GPIO.OUT)
 GPIO.setup(37, GPIO.OUT)
 #---PWM---
 GPIO.setup(40, GPIO.OUT)
+PWMB = GPIO.PWM(40, 50)
 
 #STB
 GPIO.setup(32, GPIO.OUT)
 
-#STB PWM ON
+#STB ON
 GPIO.output(32, 1)
-GPIO.output(38, 1)
-GPIO.output(40, 1)
-GPIO.output(36, 1)
 
 #Servo
 SList = 1
 ServoAngle = [0,0,0,0,0,0]
 
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 
-Servo1 = GPIO.PWM(3, 50) # GPIO 17 for PWM with 50Hz
-Servo2 = GPIO.PWM(5, 50)
-Servo3 = GPIO.PWM(7, 50)
+Servo1 = GPIO.PWM(8, 50) # GPIO 17 for PWM with 50Hz
+Servo2 = GPIO.PWM(10, 50)
+Servo3 = GPIO.PWM(12, 50)
 Servo4 = GPIO.PWM(11, 50)
 Servo5 = GPIO.PWM(13, 50)
 Servo6 = GPIO.PWM(15, 50)
@@ -65,16 +64,21 @@ Servo3.start(2.5)
 Servo4.start(2.5)
 Servo5.start(2.5)
 Servo6.start(2.5)
+PWMA.start(0)
+PWMB.start(0)
+PWMBo.start(100)
 
 
 def Motor(Left,Right):
     if Left > 0 and Right > 0:
         print("Forward")
         #Left
+        PWMA.ChangeDutyCycle(Left)
         GPIO.output(31, 1)
         GPIO.output(33, 0)
         
         #Right
+        PWMB.ChangeDutyCycle(Right)
         GPIO.output(35, 1)
         GPIO.output(37, 0)
         
@@ -82,32 +86,38 @@ def Motor(Left,Right):
     elif Left < 0 and Right < 0:
         print("Backward")
         #Left
+        PWMA.ChangeDutyCycle(Left*-1)
         GPIO.output(31, 0)
         GPIO.output(33, 1)
         
         #Right
+        PWMB.ChangeDutyCycle(Right*-1)
         GPIO.output(35, 0)
         GPIO.output(37, 1)
         
 
-    elif Left > 0 and Right < 0:
-        print("Right")
+    elif Left >= 0 and Right <= 0:
+        print("Left")
         #Left
+        PWMA.ChangeDutyCycle(Left)
         GPIO.output(31, 1)
         GPIO.output(33, 0)
         
         #Right
+        PWMB.ChangeDutyCycle(Right*-1)
         GPIO.output(35, 0)
         GPIO.output(37, 1)
         
 
-    elif Left < 0 and Right > 0:
-        print("Left")
+    elif Left <= 0 and Right >= 0:
+        print("Right")
         #Left
+        PWMA.ChangeDutyCycle(Left*-1)
         GPIO.output(31, 0)
         GPIO.output(33, 1)
         
         #Right
+        PWMB.ChangeDutyCycle(Right)
         GPIO.output(35, 1)
         GPIO.output(37, 0)
 
@@ -148,13 +158,14 @@ def ServoAPOS(x):
     if x > 180 : x = x-180
     elif x < 1 : x = 180-x
     return x
-
+message = "None"
+Motor(0,0)
 try:
     while(1):
         message = socket.recv()
         print(f"Received request: {message}")
         socket.send_string("recived")
-
+        # Motor(50,50)
         if message ==  b'ESC':
             print("Exit")
             break
